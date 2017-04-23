55c7ab4ab778db28094b7c45220aed68e5cbf265
yangoff <yangovip@gmail.com>
2017-04-23 19:20:58 -0300

    asdcda

--- src/main/java/entity/Acompanhamento.java
+++ src/main/java/entity/Acompanhamento.java
@@ -1,16 +1,26 @@
 package entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by harlock on 23/04/17.
  */
-@Entity
 public class Acompanhamento implements Serializable {
     private static final long serialVersionUID = -370016001147031854L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_ACOMPANHAMENTO")
-    private Integer idf;
+    private int id_acompanhamento;
+    private Date dtacompanhamento;
+    private String descricao;
+    private Enum<Classificacao> classificacao;
+
+    enum Classificacao{
+        Pesséssimo,Ruim,Regular,Bom,Ótimo
+    }
 }
