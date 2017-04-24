d20eae62dab1334f40d6e1c9417be5f97871aa21
yangoff <yangovip@gmail.com>
2017-04-23 22:10:09 -0300

    Service Iniciado-(Nao testado)

--- src/main/java/services/PesquisaMercadoService.java
+++ src/main/java/services/PesquisaMercadoService.java
@@ -1,7 +1,43 @@
 package services;
 
+import dao.PesquisaMercadoDAO;
+import entity.PesquisaMercado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by harlock on 23/04/17.
  */
+
+@Service
+@Transactional
 public class PesquisaMercadoService {
+
+    @Autowired
+    private PesquisaMercadoDAO dao;
+
+    @Transactional
+    public List<PesquisaMercado> getAllPesquisaMercados(){
+        return dao.retornaTodos();
+    }
+    @Transactional
+    public List<PesquisaMercado> getPesquisaMercadosByNome(Integer idPesquisamercado){
+        return dao.retornaTodos(idPesquisamercado);
+    }
+    @Transactional
+    public PesquisaMercado createPesquisaMercado(PesquisaMercado PesquisaMercado){
+        return dao.inserirPesquisaMercado(PesquisaMercado);
+    }
+    @Transactional
+    public void updatePesquisaMercado(PesquisaMercado PesquisaMercado){
+        dao.updatePesquisaMercado(PesquisaMercado);
+    }
+    @Transactional
+    public void deletePesquisaMercado(PesquisaMercado PesquisaMercado){
+        dao.deletePesquisaMercado(PesquisaMercado);
+    }
+
 }
--- src/main/java/services/PessoaService.java
+++ src/main/java/services/PessoaService.java
@@ -1,7 +1,43 @@
 package services;
 
+import dao.PessoaDAO;
+import entity.Pessoa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by harlock on 23/04/17.
  */
+
+@Service
+@Transactional
 public class PessoaService {
+
+    @Autowired
+    private PessoaDAO dao;
+
+    @Transactional
+    public List<Pessoa> getAllPessoas(){
+        return dao.retornaTodos();
+    }
+    @Transactional
+    public List<Pessoa> getPessoasByNome(Integer idPessoa){
+        return dao.retornaTodos(idPessoa);
+    }
+    @Transactional
+    public Pessoa createPessoa(Pessoa Pessoa){
+        return dao.inserirPessoa(Pessoa);
+    }
+    @Transactional
+    public void updatePessoa(Pessoa Pessoa){
+        dao.updatePessoa(Pessoa);
+    }
+    @Transactional
+    public void deletePessoa(Pessoa Pessoa){
+        dao.deletePessoa(Pessoa);
+    }
+
 }
